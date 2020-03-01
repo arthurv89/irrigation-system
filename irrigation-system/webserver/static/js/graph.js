65968e6e402958bd2312201266d412984b8cb796
AV <akmo75@gmail.com>
2020-03-01 09:31:50 +0000

    Multiple graphs, with colors and refactoring

--- irrigation-system/webserver/static/js/graph.js
+++ irrigation-system/webserver/static/js/graph.js
@@ -1,7 +1,60 @@
 function render(timeseries, id) {
+  colors = ["black", "red", "agua", "lime", "blue", "purple"]
+
+  function add_circles(line_index) {
+    svg.selectAll("myCircles")
+      .data(data)
+      .enter()
+      .append("circle")
+        .attr("fill", colors[line_index])
+        .attr("stroke", "none")
+        .attr("cx", function(d) { return x(d[0]) })
+        .attr("cy", function(d) { return y(d[line_index]) })
+        .attr("r", 3)
+  }
+
+  function append_line(line, line_index) {
+    // Add the valueline2 path.
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .style("stroke", colors[line_index])
+        .attr("d", line);
+  }
+
+  function create_line(line_index) {
+    d3.line()
+        .x(function(d) { return x(d[0]); })
+        .y(function(d) { return y(d[line_index]); })
+  }
+
+  function add_lines() {
+    // define the 1st line
+    var valueline = create_line(1);
+    var valueline2 = create_line(2);
+    var valueline3 = create_line(3);
+
+    // Add the valueline path.
+    append_line(valueline, 1)
+    append_line(valueline2, 2)
+    append_line(valueline3, 3)
+
+    // Add the line
+    add_circles(1)
+    add_circles(2)
+    add_circles(3)
+  }
+
+  function max_y(d) {
+    return Math.max(d[1], d[2], d[3])
+  }
+
     data = timeseries['rows']
     console.log(data)
 
+    for(i in data) {
+      data[i][1] *= 1000
+    }
 
     // set the dimensions and margins of the graph
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
@@ -12,20 +65,6 @@ function render(timeseries, id) {
     var x = d3.scaleTime().range([0, width]);
     var y = d3.scaleLinear().range([height, 0]);
 
-    // define the 1st line
-    var valueline = d3.line()
-        .x(function(d) { return x(d[0]); })
-        .y(function(d) { return y(d[1]); });
-
-    // define the 2nd line
-    var valueline2 = d3.line()
-        .x(function(d) { return x(d[0]); })
-        .y(function(d) { return y(d[2]); });
-
-    // define the 2nd line
-    var valueline3 = d3.line()
-        .x(function(d) { return x(d[0]); })
-        .y(function(d) { return y(d[3]); });
 
     // append the svg obgect to the body of the page
     // appends a 'group' element to 'svg'
@@ -39,27 +78,9 @@ function render(timeseries, id) {
 
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d[0]; }));
-    y.domain([0, d3.max(data, function(d) {
-  	  return Math.max(d[1], d[2], d[3]); })]);
+    y.domain([0, d3.max(data, function(d) { return max_y(d); })]);
 
-    // Add the valueline path.
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", valueline);
-
-    // Add the valueline2 path.
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .style("stroke", "red")
-        .attr("d", valueline2);
-
-    // Add the valueline path.
-    svg.append("path")
-    .data([data])
-    .attr("class", "line")
-    .attr("d", valueline3);
+    add_lines()
 
     // Add the X Axis
     svg.append("g")
@@ -71,5 +92,4 @@ function render(timeseries, id) {
     // Add the Y Axis
     svg.append("g")
         .call(d3.axisLeft(y));
-
 }
