414f76870eae914399e72127743f5666a11f30f5
Arthur Vlug <arthur.vlug@persgroep.net>
2021-09-04 20:34:15 +0100

    Revert "versioned OTA API call"
    This reverts commit 6de8aa2efdd55aacbeea8da948525c5818e256e4.

--- irrigation-system/dev/compile-midgets.sh
+++ irrigation-system/dev/compile-midgets.sh
@@ -6,21 +6,10 @@ arduinoPath="/Applications/Arduino.app/Contents/MacOS/Arduino"
 binPath="$base/webserver-pi/bin"
 echo $binPath
 
-datetime=`date '+%d-%m-%Y_%H-%M-%S'`
-version="$datetime"
-
-echo "#include \"Version.h\"
-
-String getVersion() {
-  return \"$version\";
-}
-" > $base/midgets/controller-valve/Version.cpp
-
 cd "$base/midgets/controller-valve"
+rm -rf build
 "$arduinoPath" --pref build.path=build --board esp8266:esp8266:nodemcuv2 --verify *.ino
-target="$binPath/valve/valve-$version.bin"
-echo $target
-cp build/controller-valve.ino.bin "$binPath/valve/valve-$version.bin"
+mv build/controller-valve.ino.bin $binPath/valve/real/valve.bin
 
 # cd $base/midgets/sensor/midget-moisture
 # rm -rf build
--- irrigation-system/midgets/controller-valve/Version.cpp
+++
@@ -1,6 +0,0 @@
-#include "Version.h"
-
-String getVersion() {
-  return "04-09-2021_19-42-23";
-}
-
